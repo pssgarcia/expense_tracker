836dc5cffcb3a6befe1f7b68fe97e58ba4809502
Pedro Soares <pedrossgarcia88@gmail.com>
2023-08-30 19:27:34 -0700

    first commit

---
+++ php/rest/api/V1/expense_api.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("../../../inc/config.inc.php");
+require_once("../../../inc/Entities/Expense.class.php");
+require_once("../../../inc/Utilities/PDOAgent.class.php");
+require_once("../../../inc/Utilities/DAO/ExpenseDAO.class.php");
+require_once("../../../inc/Utilities/ExpensesConverter.class.php");
+
+ExpenseDAO::startDb();
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, HEAD, OPTIONS, PUT, DELETE");
+header("Content-Type: application/json; charset=UTF-8");
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch($method) {
+   case 'GET':
+      echo json_encode(
+         ExpensesConverter::convertToStd(ExpenseDAO::getAllExpenses())
+      );
+   break;
+   case 'POST':
+      $data = json_decode(file_get_contents('php://input'));
+      ExpenseDAO::insertNewExpense(ExpensesConverter::convertToObj($data));
+      header("Location: http://localhost:8080");
+   break;
+}
---
+++ src/components/ExpenseTable.vue
@@ -0,0 +1,61 @@
+<template>
+	<article class="flex justify-center mt-8">
+		<table class="border-2 border-black shadow-xl w-8/12 text-xl">
+			<thead class="bg-green-600">
+				<tr>
+					<th class="py-3">#</th>
+					<th class="py-3">Type</th>
+					<th class="py-3">Name</th>
+					<th class="py-3">Date</th>
+					<th class="py-3">Amount</th>
+					<th></th>
+				</tr>
+			</thead>
+			<tbody class="text-center">
+				<tr
+					class="cursor-pointer border-2 border-black duration-300 bg-green-500"
+					v-for="(expense, index) in expenses"
+					:key="index"
+				>
+					<td class="py-3">
+						{{ index + 1 }}
+					</td>
+					<td class="py-3">
+						{{ expense.type }}
+					</td>
+					<td class="py-3">
+						{{ expense.name }}
+					</td>
+					<td class="py-3">
+						{{ expense.date }}
+					</td>
+					<td class="py-3">
+                  ${{ expense.amount }}
+               </td>
+					<td>
+						<img 
+                     @click="removeExpense(index)"
+                     class="w-5 h-5" 
+                     src="../assets/x.png" 
+                     alt="X" 
+                  />
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</article>
+</template>
+
+<script>
+export default {
+	name: 'ExpenseTable',
+	props: {
+		expenses: [],
+	},
+   methods: {
+      removeExpense(index) {
+         this.$emit("removeExpense", index);
+      }
+   }
+};
+</script>
